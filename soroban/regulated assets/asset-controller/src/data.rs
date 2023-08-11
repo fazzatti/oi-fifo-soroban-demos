2159936e4b1b7644ecf4004caa8fbe6f2112baeb
Fabricius Zatti <62725221+fazzatti@users.noreply.github.com>
2023-08-10 21:13:48 -0300

    invoker validation and more

--- soroban/regulated assets/asset-controller/src/data.rs
+++ soroban/regulated assets/asset-controller/src/data.rs
@@ -3,6 +3,8 @@ use crate::storage_types::{DataKey, UserActivityData, BALANCE_BUMP_AMOUNT};
 
 
 ///should add the BUMPS?
+/// persistent vs instance vs temporary
+/// 
 
 pub fn read_outflow_limit(e: &Env) -> i128 {
     let key = DataKey::OutflowLimit;
@@ -14,7 +16,7 @@ pub fn write_outflow_limit(e: &Env, amount: i128) {
     e.storage().instance().set(&key, &amount);
 }
 
-pub fn read_user_outflow(e: &Env, user: Address) -> i128 {
+pub fn read_user_outflow_quota(e: &Env, user: Address) -> i128 {
     let key = &DataKey::UserActivity(user);
     if let Some(user_data) = e.storage().instance().get::<_, UserActivityData>(key) {
         e.storage().persistent().bump(key, BALANCE_BUMP_AMOUNT);
@@ -23,5 +25,26 @@ pub fn read_user_outflow(e: &Env, user: Address) -> i128 {
         0
     }
 }
+
+pub fn write_user_outflow_quota(e: &Env, user: Address) -> i128 {
+    let key = &DataKey::UserActivity(user);
+    if let Some(user_data) = e.storage().instance().get::<_, UserActivityData>(key) {
+        e.storage().persistent().bump(key, BALANCE_BUMP_AMOUNT);
+        user_data.recent_outflow
+    } else {
+        0
+    }
+}
+
+pub fn read_user_inflow_quota(e: &Env, user: Address) -> i128 {
+    let key = &DataKey::UserActivity(user);
+    if let Some(user_data) = e.storage().instance().get::<_, UserActivityData>(key) {
+        e.storage().persistent().bump(key, BALANCE_BUMP_AMOUNT);
+        user_data.recent_inflow
+    } else {
+        0
+    }
+}
+
 
 
